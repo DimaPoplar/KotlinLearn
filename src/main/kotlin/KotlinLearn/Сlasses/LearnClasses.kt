612e656fdb308d4a800b85fcdc4d0c6db59ecd28
DimaGrishin <amvlastik@gmail.com>
2023-09-29 22:07:14 +0300

    Task (TwoSum; PalindromeNumber; RomanToInt) Learn (Classes; Type)

---
+++ src/main/kotlin/KotlinLearn/Сlasses/LearnClasses.kt
@@ -0,0 +1,12 @@
+package KotlinLearn.Сlasses
+
+fun main(args: Array<String>) {
+    val dog = Dog("Cat", "Corg")
+//    print(dog.name)
+//    print(dog.breed)
+//    dog.who()
+    println(dog.weight)
+    dog.weight = 132
+    println(dog.weight)
+
+}
